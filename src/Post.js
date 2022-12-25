af72e2b4f68896ff36a35c888681fb0f7e3bb6bc
olatorera <66270040+olatorera@users.noreply.github.com>
2022-12-26 00:47:56 +0100

    worked onthe feed

--- src/Post.js
+++ src/Post.js
@@ -1,6 +1,11 @@
 import { Avatar } from "@material-ui/core";
 import React from "react";
 import "./Post.css";
+import FeedOptions from "./resources/FeedOptions";
+import ThumbUpAltOutlinedIcon from "@material-ui/icons/ThumbUpAltOutlined";
+import ChatOutlinedIcon from "@material-ui/icons/ChatOutlined";
+import ShareOutlinedIcon from "@material-ui/icons/ShareOutlined";
+import SendOutlinedIcon from "@material-ui/icons/SendOutlined";
 
 const Post = ({ name, description, message, photoUrl }) => {
   return (
@@ -8,13 +13,20 @@ const Post = ({ name, description, message, photoUrl }) => {
       <div className="post-header">
         <Avatar />
         <div className="post-info">
-          <h2> Sanusi Olatorera</h2>
-          <p>Description</p>
+          <h2> {name} </h2>
+          <p> {description} </p>
         </div>
       </div>
 
       <div className="post-body">
-        <p>Message comes in here</p>
+        <p> {message} </p>
+      </div>
+
+      <div className="post_buttons">
+        <FeedOptions Icon={ThumbUpAltOutlinedIcon} title="Like" color="gray" />
+        <FeedOptions Icon={ChatOutlinedIcon} title="Comment" color="gray" />
+        <FeedOptions Icon={ShareOutlinedIcon} title="Share" color="gray" />
+        <FeedOptions Icon={SendOutlinedIcon} title="Send" color="gray" />
       </div>
     </div>
   );
