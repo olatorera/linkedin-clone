af8dce2bf7475420ec117c9907439781676994de
olatorera <66270040+olatorera@users.noreply.github.com>
2023-02-02 12:17:05 +0100

    styles

--- src/Leftbar.js
+++ src/Leftbar.js
@@ -1,6 +1,6 @@
 import { Avatar, Box, Button } from "@material-ui/core";
 import React from "react";
-import "./Sidebar.css";
+import "./Leftbar.css";
 import sidebar from "../src/images/sidebar.png";
 import linkedinblob from "../src/images/linkedinblob.jpeg";
 
@@ -9,44 +9,50 @@ const Sidebar = () => {
     <div className="sidebar__body">
       <div className="sidebar-top">
         <div className="sidebar-stats"></div>
-        <Box>
+        <div>
           <Avatar className="sidebar-avatar" />
           <p> Job Vacancies in Nigeria</p>
           <Button variant="outlined">Follow</Button>
-        </Box>
-        <Box>
+        </div>
+        <div>
           <Avatar className="sidebar-avatar" />
           <p> Job Vacancies in Nigeria</p>
           <Button variant="outlined">Follow</Button>
-        </Box>
-        <Box>
+        </div>
+        <div>
           <Avatar className="sidebar-avatar" />
           <p> Job Vacancies in Nigeria</p>
           <Button variant="outlined">Follow</Button>
-        </Box>
+        </div>
       </div>
 
       <div className="sidebar-bottom">
         <img src={sidebar} alt="" className="see who is hiring" />
-        <Box>
-          <p>About</p>
-          <p>Accessibility</p>
-          <p>Help Center</p>
-          <p>Privacy & Terms</p>
-          <p>Ad Choices</p>
-          <p>Advertising</p>
-          <p>Business Services</p>
-          <p>Get the LinkedIn app</p>
-          <p>More</p>
-        </Box>
-        <Box>
+        <footer className="bottom-text">
+          <div className="footer1">
+            <p>About</p>
+            <p>Accessibility</p>
+            <p>Help Center</p>
+          </div>
+          {/* <div>
+            <p>Privacy & Terms</p>
+            <p>Ad Choices</p>
+            <p>Advertising</p>
+          </div>
+          <div>
+            <p>Business Services</p>
+            <p>Get the LinkedIn app</p>
+            <p>More</p> */}
+          {/* </div> */}
+        </footer>
+        <div>
           <img
             src={linkedinblob}
             alt="see who is hiring"
             className="blob-logo"
           />
           <p>LinkedIn Corporation 2023</p>
-        </Box>
+        </div>
         <p>TeeDev </p>
       </div>
     </div>
